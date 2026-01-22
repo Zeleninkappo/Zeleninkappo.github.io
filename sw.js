646a3b196d07701482161e33c9fed7c0f8af5999
Zeleninkappo <117789853+Zeleninkappo@users.noreply.github.com>
2026-01-22 20:58:07 +0100

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,39 @@
+const CACHE_NAME = 'zelix-v0.4.2';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './data.js',
+  './logic.js',
+  './ui.js',
+  './manifest.json'
+];
+
+// Instalace Service Workeru a cachování souborů
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Aktivace a vymazání staré cache (pokud změníš verzi)
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Interceptování požadavků (Offline first strategie)
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
